b3e13ce0bd6ff8502e689c01df5af2ffe97c1187
Rites1 <103243296+Rites1@users.noreply.github.com>
2023-04-11 20:31:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,21 @@ function take_snapshot(){
     });
 }
 console.log("ml5 version: ", ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UwzRjH_Ao/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xeazkzbjs/model.json', modelLoaded);
 function modelLoaded(){
     console.log("modelLoaded");
+}
+function check(){
+    img = document.getElementById("capture_image");
+    classifier.classify(img,gotresult);
+}
+function gotresult(error,results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
